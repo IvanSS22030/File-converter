8612cabb8a5fffaf95fd01bd6fa81e44c030bac7
Ivan joel Sanchez Santana <ivanj_ss@hotmail.com>
2023-06-11 15:53:17 -0400

    Now a Button has been created

--- PDF_converter.py
+++ PDF_converter.py
@@ -1,48 +1,55 @@
-#First we need to import everything you'll use to make it work with the files of your preference. 
-import img2pdf
 import os
-from PyPDF2 import PdfFileReader, PdfFileWriter
-from docx import Document
+import tkinter as tk
+from tkinter import filedialog
 from docx2pdf import convert
 from PIL import Image
+import win32com.client
 
-
-#next we define a function to convert it to PDF 
 def convert_doc_to_pdf(input_path, output_path):
     convert(input_path, output_path)
 
-#Then we do the same, but backwards, PDF to Doc, but we use Page.extractText() to get the text from the file and then we use Doc.save (and then we provide the outpath later in the code)
-def convert_pdf_to_doc(input_path, output_path):
-    pdf = PdfFileReader(input_path)
-    doc = Document()
-#This add the new funtionality to convert JPG files to PDF. 
-    for page_num in range(pdf.numPages):
-        page = pdf.getPage(page_num)
-        text = page.extractText()
-        doc.add_paragraph(text)
+def convert_jpg_to_pdf(input_path, output_path):
+    Im_1 = Image.open(input_path)
+    Im_1.save(output_path, "PDF", quality=100)
 
-    doc.save(output_path)
-
-def convert_jpg_to_pdf(input_path,output_path):
-  Im_1=Image.open(input_path)
-  Im_1.save(output_path)  
-
-
-#We define the function that will make the file convertion 
-def convert_file(input_path, output_path):
-    file_name, file_extension = os.path.splitext(input_path)
-    file_extension = file_extension.lower()
-
-    if file_extension == ".pdf":
-        convert_pdf_to_doc(input_path, output_path)
-    if file_extension ==".jpg":
-        convert_jpg_to_pdf(input_path, output_path)   
-    elif file_extension == ".docx":
-        convert_doc_to_pdf(input_path, output_path)
+def convert_file():
+    input_file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf"), ("JPEG Files", "*.jpg"), ("Word Document", "*.docx")])
+    if input_file_path:
+        file_name, file_extension = os.path.splitext(input_file_path)
+        file_extension = file_extension.lower()
+        if file_extension == ".pdf":
+            output_file_path = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word Document", "*.docx")])
+            if output_file_path:
+                word = win32com.client.Dispatch("Word.Application")
+                word.Visible = 0
+                wb = word.Documents.Open(input_file_path)
+                wb.SaveAs2(output_file_path, FileFormat=16)
+                wb.Close()
+                print("PDF to DOCX conversion is completed.")
+        elif file_extension == ".jpg":
+            output_file_path = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF", "*.pdf")])
+            if output_file_path:
+                convert_jpg_to_pdf(input_file_path, output_file_path)
+                print("JPG to PDF conversion is completed.")
+        elif file_extension == ".docx":
+            output_file_path = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF", "*.pdf")])
+            if output_file_path:
+                convert_doc_to_pdf(input_file_path, output_file_path)
+                print("DOCX to PDF conversion is completed.")
+        else:
+            print("Unsupported file format.")
     else:
-        print("Unsupported file format.")
+        print("No file selected.")
 
-#We specify the routes, one inputh and the output, being careful of the / slashes. 
-input_file_path = "C:/Users/IvanJ/Downloads/Bankstatement.jpg"
-output_file_path = "C:/Users/Ivanj/OneDrive/Escritorio/foto.pdf"
-convert_file(input_file_path, output_file_path)
+def create_gui():
+    root = tk.Tk()
+    root.title("File Converter")
+    root.geometry("400x200")  # Set the window size (width x height)
+
+    browse_button = tk.Button(root, text="Seleccione su archivo", command=convert_file)
+    browse_button.pack(pady=10)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    create_gui()
